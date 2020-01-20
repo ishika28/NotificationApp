da538883fc9f0aff4ee660598809eb6494768bb0
ishika28 <isu.pradhan@gmail.com>
2020-01-20 11:00:58 +0545

    notification high/low

--- app/src/main/java/com/example/notificationapp/MainActivity.java
+++ app/src/main/java/com/example/notificationapp/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.notificationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private NotificationManagerCompat notificationManagerCompat;
     private Button btnnotificationone, btnnotificationtwo;
 
     @Override
@@ -16,8 +23,49 @@ public class MainActivity extends AppCompatActivity {
         btnnotificationone=findViewById(R.id.btnnitoficationone);
         btnnotificationtwo=findViewById(R.id.btnnitoficationotwo);
 
+        notificationManagerCompat = NotificationManagerCompat.from(this);
+        CreateChannel channel = new CreateChannel(this);
+        channel.createChannel();
+
+        btnnotificationone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DisplayNotification();
+            }
+        });
+
+        btnnotificationtwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DisplayNotification2();
+            }
+        });
 
 
+
+
+
+
+    }
+    private void DisplayNotification(){
+
+        Notification notification = new NotificationCompat.Builder(this, CreateChannel.CHANNEL_1)
+                .setSmallIcon(R.drawable.ic_announcement_black_24dp)
+                .setContentTitle("First Message")
+                .setContentText("First message body")
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+        notificationManagerCompat.notify(1, notification);
+    }
+
+    private void DisplayNotification2(){
+        Notification notification = new NotificationCompat.Builder(this, CreateChannel.CHANNEL_2)
+                .setSmallIcon(R.drawable.ic_message_black_24dp)
+                .setContentTitle("Second Message")
+                .setContentText("Second message body")
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+        notificationManagerCompat.notify(2, notification);
 
     }
 }
